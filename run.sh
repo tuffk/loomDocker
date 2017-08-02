45b82e668d8edd1542cfd6f1b6b29b9cdb17f056
Tuffk <tuffkmulhall@gmail.com>
2017-08-02 09:59:25 +0300

    update name on run sh

--- run.sh
+++ run.sh
@@ -3,10 +3,10 @@ then
   echo "Error starting monitor: please specify your customer name"
   exit 1
 fi
-python jsoner.py
+python update_filebeat_config.py
 filebeat -e &
 while true
 do
-  python jsoner.py
+  python update_filebeat_config.py
   sleep 5
 done
